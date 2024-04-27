db782416e21cc76f224734d846e20635dde4abee
Adam Meszaros <xmeszar2@fi.muni.cz>
2024-04-27 11:18:12 +0200

    feat: add time support

---
+++ src/constants/credentials.c
@@ -0,0 +1,4 @@
+#include "../include/credentials.h"
+
+const char* WIFI_SSID       = "YOUR_SSID";
+const char* WIFI_PASSWORD   = "YOUR_PASS";
---
+++ src/include/credentials.h
@@ -0,0 +1,7 @@
+#ifndef _CREDENTIALS_H
+#define _CREDENTIALS_H
+
+extern const char* WIFI_SSID;
+extern const char* WIFI_PASSWORD;
+
+#endif // _CREDENTIALS_H
